f4e0c6b542b017ca07f23e97fe83dc2f59a1b8c4
Duffy <web.duckdodgers@gmail.com>
2023-09-23 11:21:22 +0200

    Correction

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,5 +1,27 @@
 
-let nav = document.querySelector('nav');
+const links = document.querySelectorAll('nav li a');
+const box = document.querySelector('#box')
 
-console.log(nav);
-nav.addEventListener('click' () =>{})
+links.forEach((push) => {
+    push.addEventListener('click', () => {
+        nav.classList.remove('active-menu');
+        box.classList.toggle('action-box');
+    })
+})
+
+box.addEventListener('click', () => {
+    nav.classList.toggle('active-menu')
+    box.classList.toggle('action-box');
+})
+
+
+const sections = document.querySelectorAll('section');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let id = sec.getAttribute('id');
+        links.forEach(link => {
+            link.classList.remove('view');
+            document.querySelector(id).classList.add('view');
+        });
+    })
+};
